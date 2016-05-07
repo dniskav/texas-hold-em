3ec0663cdced1092434db2e9bdb995191ff4b7fe
Daniel Silva <dniskav@gmail.com>
2016-05-07 18:02:15 -0500

    basic card creation and shuffle

---
+++ app/js/app.js
@@ -0,0 +1,48 @@
+//(function(){
+    var Maze = function(){
+        vm = this;
+        var kinds = ['Hearts', 'Diamonds', 'Clubs', 'Spades'];
+
+        var deck = [];
+
+        deck.generate = function  () {
+            var that = this;
+            kinds.forEach(function(kind){
+                for(var i = 2; i <= 14; i++){
+                    that.push(new Card({kind: kind, value: i}))
+                }
+            });
+            return this;
+        };
+
+        deck.shuffle = function () {
+            var tempDeckO = deck.slice();
+            this.length = 0;
+            while(tempDeckO.length){
+                deck.push(tempDeckO.splice(Math.floor(Math.random() * tempDeckO.length),1)[0])
+            }
+            return this;
+        };
+
+        return deck;
+    };
+
+
+    function Card(params){
+        var vm = this;
+        var letters = {
+            11:'J',
+            12:'Q',
+            13: 'K',
+            14: 'A'
+        };
+        vm.kind = params.kind;
+        vm.value = params.value;
+        vm.display = (vm.value > 10) ? letters[vm.value] : vm.value;
+
+        vm.render = function(){
+            console.log(vm.display + ' of ' + vm.kind);
+        }
+    }
+
+//})();
